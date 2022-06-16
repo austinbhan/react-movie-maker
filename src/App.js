4b286dcc74196b0eb459c00cc6166928cd0d5bdb
austinbhan <austinbhan@outlook.com>
2022-06-16 16:43:22 -0700

    Bookform.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import Bookform from './Bookform';
 
 function App() {
 // UseState Go here
-const [title, setTitle] = useState('');
-const [author, setAuthor] = useState('');
-const [year, setYear] = useState('');
+// const [title, setTitle] = useState('');
+// const [author, setAuthor] = useState('');
+// const [year, setYear] = useState('');
 
 // Functions Go Here
 
 // Return Goes Here
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Movie Maker Project Title</h1>
-      </header>
-      <body className="body">
-        <div id="input">
-          <p>Inputs go here</p>
-          <Bookform />
-        </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Movie Maker Project Title</h1>
+        </header>
+        <section className="body">
+          <div id="input">
+            <p>Inputs go here</p>
+            <Bookform />
+          </div>
 
-        <div id="preview">
-          <p>Preview goes here</p>
-        </div>
+          <div id="preview">
+            <p>Preview goes here</p>
+          </div>
 
-        <div id="filter-and-delete">
-          <p>Filter and delete buttons go here</p>
-        </div>
+          <div id="filter-and-delete">
+            <p>Filter and delete buttons go here</p>
+          </div>
 
-        <div id="list-display">
-          <p>Display List goes here</p>
-        </div>
-      </body>
-      <footer>
+          <div id="list-display">
+            <p>Display List goes here</p>
+          </div>
+        </section>
+        <footer>
 
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   );
 }
 
--- src/Bookform.js
+++ src/Bookform.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import './App.css';
 
-export default function Bookform() {
-    
+export default function Bookform({ submit, title, setTitle, author, setAuthor, year, setYear }) {
+  return (
+    <>
+      <div className="book-form-container">
+        <form className="bookform" onChange={e => submit(e.target.value)}>
+          <p>Title
+            <input value={title} onChange={e => setTitle(e.target.value)}></input>
+          </p>    
+          <p>Author
+            <input value={author} onChange={e => setAuthor(e.target.value)}></input>
+          </p>
+          <p>Year
+            <input value={year} onChange={e => setYear(e.target.value)}></input>
+          </p>
+          <button>Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
