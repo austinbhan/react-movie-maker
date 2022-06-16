4fd891fa4f5023b36bf91ede92b8361d7330ff7c
austinbhan <austinbhan@outlook.com>
2022-06-16 16:24:55 -0700

    Added Bookform file and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import { useState } from 'react';
+import Bookform from './Bookform';
 
 function App() {
+// UseState Go here
+const [title, setTitle] = useState('');
+const [author, setAuthor] = useState('');
+const [year, setYear] = useState('');
+
+// Functions Go Here
+
+// Return Goes Here
   return (
     <div className="App">
       <header className="App-header">
@@ -8,18 +18,20 @@ function App() {
       </header>
       <body className="body">
         <div id="input">
-          <p>Input goes here</p>
+          <p>Inputs go here</p>
+          <Bookform />
         </div>
 
         <div id="preview">
           <p>Preview goes here</p>
         </div>
 
-        <div id="list-display">
-          <p>Display goes here</p>
-        </div>
         <div id="filter-and-delete">
-          <p>Filter and delete goes here</p>
+          <p>Filter and delete buttons go here</p>
+        </div>
+
+        <div id="list-display">
+          <p>Display List goes here</p>
         </div>
       </body>
       <footer>
