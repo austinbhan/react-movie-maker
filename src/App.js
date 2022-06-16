10c104087a40d4c0ec933e7c50e7511ed552f6f9
austinbhan <austinbhan@outlook.com>
2022-06-16 15:48:15 -0700

    CSS organization

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Movie Maker Project</h1>
+        <h1>Movie Maker Project Title</h1>
       </header>
-      <body>
+      <body className="body">
+        <div id="input">
+          <p>Input goes here</p>
+        </div>
 
+        <div id="preview">
+          <p>Preview goes here</p>
+        </div>
+
+        <div id="list-display">
+          <p>Display goes here</p>
+        </div>
+        <div id="filter-and-delete">
+          <p>Filter and delete goes here</p>
+        </div>
       </body>
       <footer>
-        
+
       </footer>
     </div>
   );
